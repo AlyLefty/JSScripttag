7276ee51eedf5364e91adf9e974f0b71e92f8d5a
AlyLefty <151747936+AlyLefty@users.noreply.github.com>
2023-11-22 14:24:26 +0100

    Add files via upload

--- scripttag.js
+++ scripttag.js
@@ -24,18 +24,18 @@ function getCookie(name) {
 console.log('XX');
 const url_string = window.location.href;
 const url_object = new URL(url_string);
-const my_param = url_object.searchParams.get("lefty");
+const my_param = url_object.searchParams.get("ref");
 
 console.log(my_param);
 if (my_param != null) {
 
     //set cookie
-    setCookie('lefty', my_param);
+    setCookie('ref', my_param);
 
 } else {
 
     //get cookie
-    let my_param_cookie = getCookie('lefty');
+    let my_param_cookie = getCookie('ref');
 
     if (my_param_cookie) {
 
@@ -45,7 +45,7 @@ if (my_param != null) {
         const url_pathname = url_object.pathname;
         const url_params = url_object.search;
         const url_seperator = url_params == '' ? '?' : '&';
-        const url_my_param = my_param == null ? 'lefty=' + my_param_cookie : my_param;
+        const url_my_param = my_param == null ? 'ref=' + my_param_cookie : my_param;
 
         url_all_params = url_pathname + url_params + url_seperator + url_my_param;
 
